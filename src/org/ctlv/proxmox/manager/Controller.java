907cede229f0c701e6dab3e415af1abb89c1bdd7
CHENTing <krystal_0902@icloud.com>
2019-01-20 17:07:19 +0100

    completed two functions in controller

--- src/org/ctlv/proxmox/manager/Controller.java
+++ src/org/ctlv/proxmox/manager/Controller.java
@@ -1,6 +1,15 @@
 package org.ctlv.proxmox.manager;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.security.auth.login.LoginException;
+
+import org.ctlv.proxmox.api.Constants;
 import org.ctlv.proxmox.api.ProxmoxAPI;
+import org.ctlv.proxmox.api.data.LXC;
+import org.json.JSONException;
 
 public class Controller {
 
@@ -10,13 +19,33 @@ public class Controller {
 	}
 	
 	// migrer un conteneur du serveur "srcServer" vers le serveur "dstServer"
-	public void migrateFromTo(String srcServer, String dstServer)  {
-//		api.migrateCT(srcServer, ctID, dstServer);
+	public void migrateFromTo(String srcServer, String dstServer) throws LoginException, JSONException, IOException  {
+		List<LXC> cts = api.getCTs(srcServer);
+		List<LXC> myCts = new ArrayList<>();
+		
+		for (LXC ctn : cts) {
+			if ((Constants.CT_BASE_ID / 100) == (Integer.parseInt(ctn.getVmid()) / 100)) {
+				myCts.add(ctn);
+			}
+		}
+		
+		String ctID = myCts.get(myCts.size() - 1).getVmid();
+		api.migrateCT(srcServer, ctID, dstServer);
 	}
 
 	// arr�ter le plus vieux conteneur sur le serveur "server"
-	public void offLoad(String server) {
-//		api.stopCT(server, ctID);
+	public void offLoad(String server) throws LoginException, JSONException, IOException {
+		List<LXC> cts = api.getCTs(server);
+		List<LXC> myCts = new ArrayList<>();
+		
+		for (LXC ctn : cts) {
+			if ((Constants.CT_BASE_ID / 100) == (Integer.parseInt(ctn.getVmid()) / 100)) {
+				myCts.add(ctn);
+			}
+		}
+		
+		String ctID = myCts.get(0).getVmid();
+		api.stopCT(server, ctID);
 	}
 
 }
